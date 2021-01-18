c88b5d6916bdb62f9fa5af0f1dfa564b58d1fe25
shajjadKabir <sajjadkabir71@gmail.com>
2021-01-18 11:27:11 +0600

    js

---
+++ index.js
@@ -0,0 +1,18 @@
+var arrName = ['Rahim', 'Karim', 'Rafiq', 'Jabbar'];
+console.log(arrName.length);
+
+arrName.push('Shafiq');
+console.log(arrName);
+
+arrName.pop();
+console.log(arrName);
+
+arrName.shift();
+console.log(arrName);
+
+arrName.unshift('tamim');
+console.log(arrName);
+
+
+arrName.unshift('shakib');
+console.log(arrName);
